f051f43c2e47e3f98e1659b909d6132f51a20464
lktgt15 <64021540+lktgt15@users.noreply.github.com>
2020-08-29 18:22:05 +0900

    Create Java 내에서 cmd 실행.java

---
+++ Java 내에서 cmd 실행.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            Process process = Runtime.getRuntime().exec("cmd");
+
+            OutputStream stdin = process.getOutputStream();
+            InputStream stderr = process.getErrorStream();
+            InputStream stdout = process.getInputStream();
+
+            Executors.newCachedThreadPool().execute(()->{
+               try(BufferedReader reader = new BufferedReader(new InputStreamReader(stdout,"euc-kr"))){
+                   String line;
+                   while((line = reader.readLine()) != null){
+                       System.out.println(line);
+                   }
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+            });
+
+            Executors.newCachedThreadPool().execute(()->{
+                try(BufferedReader reader = new BufferedReader(new InputStreamReader(stderr,"euc-kr"))){
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        System.out.println("err "+line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            Executors.newCachedThreadPool().execute(()->{
+                try(Scanner scan = new Scanner(System.in)){
+                    try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin, "euc-kr"))){
+                        while(true){
+                            String input = scan.nextLine();
+                            input += "\n";
+                            writer.write(input);
+                            writer.flush();
+                            if("exit\n".equals(input)){
+                                System.exit(0);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
